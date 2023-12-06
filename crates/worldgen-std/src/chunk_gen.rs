78c1425c331416682f06f151c306ca1c27b530ec
Nils <nils.mathieu.contact@gmail.com>
2023-12-06 10:05:00 +0100

    remove warnings

--- crates/worldgen-std/src/chunk_gen.rs
+++ crates/worldgen-std/src/chunk_gen.rs
@@ -1,15 +1,30 @@
+use std::sync::OnceLock;
+
 use bns_core::ChunkPos;
 
 /// Contains information about a chunk that's in the process of being generated.
 pub struct ChunkGen {
     /// The position of the chun being generated.
     pos: ChunkPos,
+    /// When set, indicates that the chunk has requested the structures that it needs to spawn
+    /// from nearby biomes.
+    structures: OnceLock<()>,
 }
 
 impl ChunkGen {
     /// Creates a new [`ChunkGen`] with the provided position.
     #[inline]
     pub fn new(pos: ChunkPos) -> Self {
-        Self { pos }
+        Self {
+            pos,
+            structures: OnceLock::new(),
+        }
+    }
+
+    /// Ensures that the chunk has has requested the structures that it needs to spawn.
+    pub fn structures(&self) {
+        self.structures.get_or_init(|| {
+            let _ = self.pos;
+        });
     }
 }
